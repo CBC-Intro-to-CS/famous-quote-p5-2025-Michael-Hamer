45508d9282af97477b45c0b5055a6aacaffcd495
Michael <hamerm27@cbchs.org>
2025-08-29 09:23:46 -0500

    learned about iteration

--- iteration.py
+++ iteration.py
@@ -18,10 +18,32 @@
 #         print("Your ticket is $15.00")
 #     elif age == 0:
 #         purchase_tickets = False
-
 # print("Thank you for purchasing tickets!")
 
-count = 0
-for nums in [3, 41, 12, 9, 74, 15]:
-    count = count + 1 
-print("Count: " + str(count))
+# count = 0
+# for nums in [3, 41, 12, 9, 74, 15]:
+#     count = count + 1 
+# print("Count: " + str(count))
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#     print(f"{magician.title()} is a magician.")
+
+# print("Thank you, everyone. That was a good show!")
+
+#Using the Range function
+for value in range(1, 6):
+    print(value)
+
+numbers = list(range(1,6))
+print(numbers)
+
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+
+print(squares)
